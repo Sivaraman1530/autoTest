3835c1f5b754bb97b65b734a414d203cea8b30fd
sivaraman <sivaraman.p@fr8.in>
2024-02-27 15:45:41 +0530

    check build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -12,7 +12,7 @@ const config: CapacitorConfig = {
       "autoDeleteFailed": true,
       "autoDeletePrevious": true,
 			"autoUpdate": false,
-      "version":7.0,
+      "version":8.0,
       "defaultChannel": "production"
 		}
 	}
