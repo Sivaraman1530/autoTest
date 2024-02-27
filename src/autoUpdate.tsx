d36a6ccc4dcb9d0458b740b505eadfa0fb564364
sivaraman <sivaraman.p@fr8.in>
2024-02-27 16:26:05 +0530

    commented

--- src/autoUpdate.tsx
+++ src/autoUpdate.tsx
@@ -41,7 +41,7 @@ const InitializeApp= ()=> {
   // )
 
   // return null
-  CapacitorUpdater.addListener("updateAvailable",()=>{
+  // CapacitorUpdater.addListener("updateAvailable",()=>{
   useEffect(()=>{
   const downloadAndActivate = async () => {
     CapacitorUpdater.notifyAppReady()
@@ -64,7 +64,7 @@ console.log("ver",JSON.stringify(version))
     
   }
   downloadAndActivate();
-},[])
+// },[])
   })
   // Ensure download and activation occur when the app is initialized
  
